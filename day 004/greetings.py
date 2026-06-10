1f49ec01930b534200eccd0501fc39b72fe04518
Rex Mzumara <rexmzumara4@gmail.com>
2026-06-10 15:11:13 +0200

    Day 004: Introduction to Python Lists
    Concepts practiced:
    - Creating lists
    - Accessing list elements by index
    - Using list data in print statements
    - Basic string concatenation with lists

---
+++ day 004/greetings.py
@@ -0,0 +1,6 @@
+names = ['James','Dave','Josh']
+
+print("Your name is" + " " + names[0])
+print("Your name is" + " " + names[1])
+print("Your name is" + " " + names[2])
+
---
+++ day 004/names.py
@@ -0,0 +1,4 @@
+names = ['James','Dave','Josh']
+print(names[0])
+print(names[1])
+print(names[2])
